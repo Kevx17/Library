b1d66bfff2d764fc57740ea27e54d3310c8b8930
Koller Kevin <kevinkekoller1@gmail.com>
2024-06-10 18:01:46 +0200

    added modal and function for adding books

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 const myLibrary = [];
 
+let modal = document.querySelector('.modal');
+let addBookButton = document.querySelector('#addBook');
+let submitButton = document.querySelector('#button');
+
 function Book(title, author, pages, read) {
     this.title = title;
     this.author = author;
@@ -7,32 +11,39 @@ function Book(title, author, pages, read) {
     this.read = read;
 }
 
-let btitle = 'Nigga';
-let bauthor = 'Kevin';
-let bpages = '248';
-let bread = false;
-
-const myBook = new Book(btitle, bauthor, bpages, bread);
-
-function AddToLibrary(b) {
-    myLibrary.push(b);
-}
-
-AddToLibrary(myBook);
-
-console.log(myLibrary);
+let bTitle = '';
+let bAuthor = '';
+let bPages = '';
+let bRead = true;
 
 
-document.getElementById('addBook').onclick = () => {
-    document.getElementById('form').style.visibility = 'visible';
-}
 
-let submit = document.getElementById('submit');
 
-submit.addEventListener("click", checkboxClick, false);
+addBookButton.onclick = function() {
+    modal.showModal();
+};
 
-function checkboxClick(event) {
-  let warn = "preventDefault() won't let you check this!<br>";
-  document.getElementById("output-box").innerHTML += warn;
-  event.preventDefault();
-}
+submitButton.onclick = function() {
+    // Get form field values
+    let bTitle = document.querySelector('#title').value;
+    let bAuthor = document.querySelector('#author').value;
+    let bPages = document.querySelector('#pages').value;
+    let bRead = document.querySelector('#read').checked;
+
+    // Check if form is filled out properly
+    if (bTitle !== '' && bAuthor !== '' && bPages !== '') {
+        // Clear form fields
+        document.querySelector('#title').value = '';
+        document.querySelector('#author').value = '';
+        document.querySelector('#pages').value = '';
+        document.querySelector('#read').checked = false;
+
+        // Add new book to myLibrary
+        myLibrary.push(new Book(bTitle, bAuthor, bPages, bRead));
+
+        console.log(myLibrary);
+        modal.close();
+    }
+};
+
+
